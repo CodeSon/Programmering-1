0adce34d05b16d94a4f6483c4a75a0b6eb1216de
David Nartey <david.nartey.com>
2014-11-26 19:01:46 +0100

    Comments
    Improved the comments

--- TriangleAndItsCircles/src/TriangleAndItsCircles.java
+++ TriangleAndItsCircles/src/TriangleAndItsCircles.java
@@ -14,39 +14,43 @@ public class TriangleAndItsCircles {
 
 		System.out.println("TRIANGLE CALCULATIONS\n");
 		Scanner in = new Scanner(System.in);
-		
 
-		
-		// retrieving and storing user input to calculate the radius of the circumcircle of a triangle
-		System.out.println("Please enter the lenght of the first side of the triangle");
+		// retrieving and storing user input to calculate the radius of the
+		// circumscribed circle of a triangle
+		System.out
+				.println("Please enter the lenght of the first side of the triangle");
 		double sideA = in.nextDouble();
-		System.out.println("Please enter the lenght of the second side of the triangle");
+		System.out
+				.println("Please enter the lenght of the second side of the triangle");
 		double sideB = in.nextDouble();
-		
-		
-		
-		System.out.println("Please enter the lenght of the third side of the triangle");
+		System.out
+				.println("Please enter the lenght of the third side of the triangle");
 		double sideC = in.nextDouble();
 		// computing and printing the outer radius
 
-		double circumcircleRadius = Triangle.circumcircleRadius(sideA, sideB, sideC);
-		System.out.println("The radius of the triangle's circumcircle is " + circumcircleRadius + "\n"  );
+		double circumcircleRadius = Triangle.circumcircleRadius(sideA, sideB,
+				sideC);
+		System.out.println("The radius of the triangle's circumcircle is "
+				+ circumcircleRadius + "\n");
 		System.out.println("whiles");
 		System.out.println();
-		//computing and printing the inner radius
-		
+		// computing and printing the inner radius
+
 		double innerCircleR = Triangle.innerCircleRadius(sideA, sideB, sideC);
-		System.out.println("the radius of the triangle's inner circle is " + innerCircleR);
-		
-		//perimeter of a triangle
+		System.out.println("the radius of the triangle's inner circle is "
+				+ innerCircleR);
+
+		// perimeter of a triangle
 		double sides = Triangle.perimeter(sideA, sideB, sideC);
-		System.out.println("The perimeter of the riangle is "+ sides);
-		
-		//Bisector of the triangle
-		System.out.println("the bisector of the triangle "+ Triangle.bisector(sideA, sideB,Math.PI/(2)));
-		 
-		//the area of a triangle
-		System.out.println("The area of the triangle is " + Triangle.area(sideA, sideB, sideC));
-		
-		}
+		System.out.println("The perimeter of the riangle is " + sides);
+
+		// Bisector of the triangle
+		System.out.println("the bisector of the triangle "
+				+ Triangle.bisector(sideA, sideB, Math.PI / (2)));
+
+		// the area of a triangle
+		System.out.println("The area of the triangle is "
+				+ Triangle.area(sideA, sideB, sideC));
+
 	}
+}
